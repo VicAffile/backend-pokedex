977e1a0b02507f99f94f5cc024022f7a11435b6c
VicAffile <victoraffile@gmail.com>
2021-12-15 00:32:37 +0100

    Ajustement modèle base de donnée

--- serveur.js
+++ serveur.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+
 const db = require('./bdd/bdd');
 const Pokemon = require('./bdd/pokemon');
 
@@ -16,7 +16,6 @@ app.get('/', async(req, res) => {
         });
     });
     const reponse = JSON.stringify(liste);
-    console.log(reponse);
     res.send(reponse);
 });
 
@@ -27,6 +26,18 @@ app.get('/:pokemon', async(req, res) => {
     console.log(reponse);
     res.send(reponse);
 });
+
+app.get('/:pokemon/mignature', async(req, res) => {
+    const nom = req.params.pokemon;
+    const pokemon = await Pokemon.recuperer_pokemon(nom);
+    res.sendFile("publique/images/mignatures/" + pokemon[0].mignature, { root: __dirname });
+})
+
+app.get('/:pokemon/sprite', async(req, res) => {
+    const nom = req.params.pokemon;
+    const pokemon = await Pokemon.recuperer_pokemon(nom);
+    res.sendFile("publique/images/sprites/" + pokemon[0].sprite, { root: __dirname });
+})
 
 app.listen(process.env.PORT || 8000);
 console.log("L'application tourne.");
